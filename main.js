e521fdcb64ae5d230f581f5500fe14bce2ded777
venkat <bvenkatr2@gmail.com>
2024-02-07 22:20:49 +0530

    add a comment on what event loop checks to continue it loop or exit its loop

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/*
+
+Event Loop check these 3 conditions before exiting  the event loop
+1. pendingTimers ---- Any Pending setTimeout, setInterval, setImmediate?
+2. pendingOSTasks ---- Any Pending OS Tasks? (Like server listening to port)
+3. pendingOperaitons --- Any Pending long running operations? (Like fs module)
+
+*/
+
+
 process.env.UV_THREADPOOL_SIZE = 1;
 
 const https = require('https');
